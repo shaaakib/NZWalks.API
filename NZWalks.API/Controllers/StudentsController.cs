116b235f3b911ac1c3f185fa502c5d8c1d047a5c
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2024-11-15 07:01:04 +0600

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:protnumber/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GEt: // https://localhost:protnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[]
+            {
+                "John", "Jane", "Mark", "Emaily", "David"
+            };
+
+            return Ok(studentNames);
+        }
+    }
+}
