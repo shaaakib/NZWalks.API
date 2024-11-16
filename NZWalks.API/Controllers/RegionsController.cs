6e165dc42f4ee6f3ae3f7d5de5314aa7f6254880
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2024-11-16 19:21:28 +0600

    Change Methods To Use DTOs

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -21,10 +22,27 @@ namespace NZWalks.API.Controllers
         // https://localhost:1234/api/regions
         [HttpGet]
         public IActionResult GetAll()
-        {
-            var regions = dbContext.Regions.ToList();
+        {  
+            // Get Data From Database - Domain models
+            var regionsDomain = dbContext.Regions.ToList();
 
-            return Ok(regions);
+            // Map Dmain Models to DTOS
+
+            var regionsDto = new List<RegionDto>();
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+
+                });
+
+            }
+                // Return DTOs
+                return Ok(regionsDto);
         }
 
         // GET SINGLE REgion (Get Region by ID)
@@ -34,16 +52,27 @@ namespace NZWalks.API.Controllers
         [Route("{id:Guid}")]
         public IActionResult GetById([FromRoute]Guid id)
         {
-            var region =  dbContext.Regions.Find(id);
-
+            // Get Region Dmain Model From Database
+            var regionDomain =  dbContext.Regions.Find(id);
             //var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
            
-           if(region == null)
+           if(regionDomain == null)
             {
                 return NotFound();
             }
 
-           return Ok(region);
+            // Map/Convert Region Domain Model to Region DTO
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+
+           // Return DTO back to client
+           return Ok(regionDto);
 
         }
 
