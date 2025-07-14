46ff0430047da601a506e8dbfcb74ee0ee46ffcf
Shakib <shakibhossainmuhammad@gmail.com>
2025-07-14 07:36:47 +0600

    Filtering

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -35,9 +35,9 @@ namespace NZWalks.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
         {
-            var walksDomainModel = await walkRepository.GetAllAsync();
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery);
 
             // Map Doamin Model to Dto
 
--- NZWalks.API/Repository/Interface/IWalkRepository.cs
+++ NZWalks.API/Repository/Interface/IWalkRepository.cs
@@ -5,7 +5,7 @@ namespace NZWalks.API.Repository.Interface
     public interface IWalkRepository
     {
         Task<Walk> CreateAsync(Walk walk);
-        Task<List<Walk>> GetAllAsync();
+        Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null);
         Task<Walk?> GetByIdAsync(Guid id);
         Task<Walk?>UpdatedAsync(Guid id, Walk walk);
         Task<Walk?> DeleteAsync(Guid id);
